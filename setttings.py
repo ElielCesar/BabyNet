72c68a23f7b905539ba9761a438d3b8cf5233649
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-07-16 01:09:46 -0400

    Update setttings.py

--- setttings.py
+++ setttings.py
@@ -1,4 +1,5 @@
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,5 +80,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
