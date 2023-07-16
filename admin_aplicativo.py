d4f001afdb0243d2bfedac536600e4c3875f84ff
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-07-16 15:41:23 -0400

    Update admin_aplicativo.py

--- admin_aplicativo.py
+++ admin_aplicativo.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Presente
+from .models import Presente, PessoaPresente
 
 # Register your models here.
 class PresenteAdmin(admin.ModelAdmin):
@@ -8,3 +8,9 @@ class PresenteAdmin(admin.ModelAdmin):
 
 admin.site.register(Presente, PresenteAdmin)
 
+class PessoaPresenteAdmin(admin.ModelAdmin):
+    list_display = ['nome_pessoa', 'nome_presente', 'quantidade_presenteada']
+    ordering = ['nome_presente']
+    search_fields = ['nome_pessoa'] # não pode ser presente por causa da chave estrangeira
+
+admin.site.register(PessoaPresente, PessoaPresenteAdmin)
