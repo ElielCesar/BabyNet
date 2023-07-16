e1ff3ef2ebd84b997df48c15d1f5c594009f7991
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-07-16 01:11:50 -0400

    Update urls_aplicativo.py
    update para poder adicionar imagens no banco

--- urls_aplicativo.py
+++ urls_aplicativo.py
@@ -1,3 +1,5 @@
+from django.conf import settings                
+from django.conf.urls.static import static      
 from django.urls import path, include
 from . import views
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     # qualquer outra url jogue para 404
     path('<path:slug>/', views.page_not_found),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
