d6efabe649c9e9cac2b5e2790e02def2dacc0a4e
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-07-16 15:45:36 -0400

    Update urls_aplicativo.py

--- urls_aplicativo.py
+++ urls_aplicativo.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     # joga diretamente para a home
     path('', views.home, name='home'),
+    path('presentear/', views.presentear, name='presentear'),
+    path('valida_cadastro_presente/', views.valida_cadastro_presente, name='valida_cadastro_presente'),
     # qualquer outra url jogue para 404
     path('<path:slug>/', views.page_not_found),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
