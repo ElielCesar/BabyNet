69054169fd1e166ee27193b7e3afd0723b9a0714
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-07-16 10:39:00 -0400

    Update views_aplicativo.py

--- views_aplicativo.py
+++ views_aplicativo.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .models import Presente
+from .forms import MF_CadastrarPresente
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    presentes = Presente.objects.all()
+    return render(request, 'index.html', {'presentes':presentes})
 
 def page_not_found(request, slug):
     return render(request, '404.html', status=404)
+
+
+def presentear(request):
+    form = MF_CadastrarPresente()
+    return render(request, 'presentear.html', {'form':form})
+
+
+
+
