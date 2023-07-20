52b1aaf0cfb49ade94c8b0cad20f4d00d2f3b701
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-07-20 16:47:04 -0400

    Create urls_projeto.py

---
+++ urls_projeto.py
@@ -0,0 +1,9 @@
+# No final do arquivo urls do PROJETO, adicione as seguintes linhas:
+
+admin.site.site_header = 'BabyNet: Plataforma de Chá de Bebê Online'
+
+admin.site.site_title = 'BabyNet'
+
+admin.site.index_title = 'BabyNet - Admin'
+
+
